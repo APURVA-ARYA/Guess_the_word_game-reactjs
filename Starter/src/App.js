0e82b3d43087fb80d274077635e62350c1346080
APURVA-ARYA <66211553+APURVA-ARYA@users.noreply.github.com>
2021-07-26 06:38:38 +0530

    Add files via upload

---
+++ Starter/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './component/Header';
+import Figure from './component/Figure';
+import Face from './component/Face';
+
+
+function App() {
+  return (
+   <div>
+  <Header/>
+  <Face/>
+   </div>
+  );
+}
+
+export default App;
