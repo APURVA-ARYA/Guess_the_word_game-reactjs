d87d25819fc5cb9fb516aa3afab0e9093df5b0a3
APURVA-ARYA <66211553+APURVA-ARYA@users.noreply.github.com>
2021-07-26 06:54:32 +0530

    Update App.js

--- Starter/src/App.js
+++ Starter/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './component/Header';
 import Figure from './component/Figure';
-import Face from './component/Face';
+
 
 
 function App() {
   return (
    <div>
   <Header/>
-  <Face/>
+  <Figure/>
    </div>
   );
 }
